a8af216b35ff54c9aad6b9bc528e844c3fc6669e
mads309f <2mads.v.jensen@gmail.com>
2023-05-02 15:27:03 +0200

    animationer

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -39,15 +39,16 @@ const Filter = () => {
                 <div className="group inline-block">
                     <button type="button" className="inline-flex justify-center items-center gap-x-1 rounded-3xl bg-white px-3 py-2 text-base font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                         Tilføj
-                        <FiPlus className="text-black" />
+                        <span className='transform group-hover:-rotate-180 transition duration-300 ease-in-out'><FiPlus className="text-black" /></span>
+                        
                     </button>
                 </div>
             </div>
 
             <form>
-                <div className="relative text-center">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none ">
-                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                <div className="group inline-block relative text-center">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-non ">
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:scale-110 transition duration-200 ease-in-out" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </div>
                     <input type="" id="default-search" className="block p-2.5 w-58 pl-10 pr-14 z-20 text-sm bg-white shadow-sm rounded-3xl border border-gray-300 outline-none"
                         placeholder={"Søg efter " + fruits[active].toLowerCase()} required />
