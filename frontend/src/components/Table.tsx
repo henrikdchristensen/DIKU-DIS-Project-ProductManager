6f2e00b49e3ddc9a7601aec716c30d602bfcf64b
Henrik Christensen <henrikdchristensen@gmail.com>
2023-05-18 21:53:00 +0200

    remove svg and replace with heroicons

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { properties } from '../App';
+import { ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
 const Table = (props: properties) => {
   const [data, setData] = useState<any[]>([]);
@@ -18,7 +19,6 @@ const Table = (props: properties) => {
       let t = '';
       if (table === 0) t = 'product_templates';
       else if (table === 1) t = 'produced_products';
-
       const response = await axios.get(`/api/data?table=${t}&offset=${offset}`);
       const newData = response.data.data;
       setData((prevData) => [...prevData, ...newData]);
@@ -29,6 +29,10 @@ const Table = (props: properties) => {
       setData([]);
     }
   };
+
+  const columns = props.listActive === 0
+    ? ['Id', 'Name', 'Journal', 'Owned by']
+    : ['Serial number', 'Of Type', 'Date', 'Produced by'];
 
   const handleScroll = () => {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -48,51 +52,35 @@ const Table = (props: properties) => {
     };
   }, [handleScroll]);
 
-  
-
-    return (
-        <div className='px-10'>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg m-auto max-w-[70rem] border border-gray-200">
-                <table className="w-full text-sm text-left text-gray-500">
-                    <thead className="text-xs text-gray-700 uppercase bg-transparent border-b border-gray-200">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Id
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Name
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" /></svg></a>
-                                </div>
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Journal
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" /></svg></a>
-                                </div>
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Owned by
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" /></svg></a>
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((row, index) => (
-                        <tr key={index} className="bg-white border-b">
-                            <td>{row[0]}</td>
-                            <td>{row[1]}</td>
-                            <td>{row[2]}</td>
-                            <td>{row[3]}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  return (
+    <div className='px-10'>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg m-auto max-w-[70rem] border border-gray-200">
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-transparent border-b border-gray-200">
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index} scope="col" className="px-6 py-3">
+                  <div className="flex items-center">
+                    {column}
+                       <ChevronUpDownIcon className="h-4 w-4 ml-1" />
+                    </div>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+                <tr key={index} className="bg-white border-b">
+                {row.map((value: any, idx: number) => (
+                    <td key={idx}>{value}</td>
+                ))}
+                </tr>
+            ))}
+            </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Table
